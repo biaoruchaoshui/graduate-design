244285eccd2a8f9262f252848906de951b1efad9
biaoruchaoshui <1175836911@qq.com>
2020-05-01 19:50:48 +0800

    引入前端截图功能

--- fgqy/fg_jietu.py
+++ fgqy/fg_jietu.py
@@ -7,7 +7,32 @@ import numpy as np
 from fgqy.fg_test import fg_run
 
 
-def run_fgqy(src, style, index_lists):
+# def run_fgqy(src, style, index_lists):
+#     """
+#      风格迁移方法：
+#      @src: 原图片路径
+#      @style: 指定迁移风格
+#      @index_list: 待迁移区域的坐标数组
+#     """
+#     img = cv.imread(src)
+#     x_min = min(index_lists[0])
+#     x_max = max(index_lists[0])
+#     y_min = min(index_lists[1])
+#     y_max = max(index_lists[1])
+#     print('x_min, x_max, y_min, y_max', x_min, x_max, y_min, y_max)
+#     img_part = img[x_min: x_max, y_min: y_max]
+#     part_src = fg_run(style, img_part)
+#     result_img = cv.imread(part_src)
+#     rows, cols, tun = result_img.shape
+#     print('rows = %d, cols = %d, tun = %d', (rows, cols, tun))
+#     for index in range(len(index_lists[0])):
+#         x = index_lists[0][index]
+#         y = index_lists[1][index]
+#         for c in range(3):
+#             img[x, y, c] = result_img[x - 1 - x_min , y - y_min - 1 , c]
+#     cv.imwrite(src, img)
+
+def run_fgqy(src, style):
     """
      风格迁移方法：
      @src: 原图片路径
@@ -15,42 +40,43 @@ def run_fgqy(src, style, index_lists):
      @index_list: 待迁移区域的坐标数组
     """
     img = cv.imread(src)
-    x_min = min(index_lists[0])
-    x_max = max(index_lists[0])
-    y_min = min(index_lists[1])
-    y_max = max(index_lists[1])
-    print('x_min, x_max, y_min, y_max', x_min, x_max, y_min, y_max)
-    img_part = img[x_min: x_max, y_min: y_max]
-    part_src = fg_run(style, img_part)
-    result_img = cv.imread(part_src)
-    rows, cols, tun = result_img.shape
-    print('rows = %d, cols = %d, tun = %d', (rows, cols, tun))
-    for index in range(len(index_lists[0])):
-        x = index_lists[0][index]
-        y = index_lists[1][index]
-        for c in range(3):
-            img[x, y, c] = result_img[x - 1 - x_min , y - y_min - 1 , c]
-    cv.imwrite(src, img)
+    img_part = img[499: 785, 438: 585]
+    # img_part = img[438: 585, 499: 785]
+    fg_run(style, img_part)
+
+
+# def run_fgqy(src, style, x_min, x_max, y_min, y_max):
+#     """
+#      风格迁移方法：
+#      @src: 原图片路径
+#      @style: 指定迁移风格
+#      @index_list: 待迁移区域的坐标数组
+#     """
+#     img = cv.imread(src)
+#     img_part = img[x_min: x_max, y_min: y_max]
+#     # img_part = img[438: 585, 499: 785]
+#     return fg_run(style, img_part)
 
 
 if __name__ == '__main__':
-    lists = [[] for i in range(2)]
-    for i in range(300, 700):
-        if i % 2 == 0:
-            left = 200 + random.randint(100, 299)
-            right = 500 + random.randint(0, 100)
-        else:
-            left = 500 - random.randint(0, 50)
-            right = 700 - random.randint(0, 200)
-
-        for j in range(left, right):
-            lists[0].append(i)
-            lists[1].append(j)
-
-    print(lists[0])
-    print(lists[1])
-    print(len(lists[0]))
-    print(len(lists[1]))
-    run_fgqy('image_content/bizhi1.jpg', 'muse', lists)
+    # lists = [[] for i in range(2)]
+    # for i in range(300, 700):
+    #     if i % 2 == 0:
+    #         left = 200 + random.randint(100, 299)
+    #         right = 500 + random.randint(0, 100)
+    #     else:
+    #         left = 500 - random.randint(0, 50)
+    #         right = 700 - random.randint(0, 200)
+    # 
+    #     for j in range(left, right):
+    #         lists[0].append(i)
+    #         lists[1].append(j)
+    # 
+    # print(lists[0])
+    # print(lists[1])
+    # print(len(lists[0]))
+    # print(len(lists[1]))
+    # run_fgqy('image_content/bizhi1.jpg', 'muse', lists)
+    run_fgqy('image_content/xzb.png', 'wave')
 
 # 用一个二维数组存储每个像素点的坐标。对截图的部分进行处理后，在填充回指定位置
--- fgqy/fg_test.py
+++ fgqy/fg_test.py
@@ -12,30 +12,28 @@ def the_current_time():
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))))
 
 
-# 图片风格迁移方法 style:迁移风格， content_image： 内容图片
-def  fg_run(style, X_image):
+# 图片风格迁移方法 style:迁移风格， X_image：图像像素点数组
+def fg_run(style, X_image):
     model = 'saver_%s' % style
     result_image = 'image_output/test_%s.jpg' % style
-
+    # result_image = 'static/file/test_%s.jpg' % style
+    #  开启会话
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
-
+    # 传入模型
     saver = tf.train.import_meta_graph(os.path.join(model, 'fast_style_transfer.meta'))
     saver.restore(sess, tf.train.latest_checkpoint(model))
-
     graph = tf.get_default_graph()
     X = graph.get_tensor_by_name('X:0')
     g = graph.get_tensor_by_name('transformer/g:0')
     # 开始时间
     the_current_time()
-
     gen_img = sess.run(g, feed_dict={X: [X_image]})[0]
     gen_img = np.clip(gen_img, 0, 255) / 255.
     imsave(result_image, gen_img)
     # 结束时间
     the_current_time()
     return result_image
-
 
 
 if __name__ == '__main__':
